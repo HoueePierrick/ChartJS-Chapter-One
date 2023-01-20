fbaafa102baaa77a7105dcbefda19837b6fa960b
Pierrick Houée <houee.pierrick@gmail.com>
2023-01-20 22:12:44 +0100

    Started working on advanced drill-down chart

---
+++ browserdata.js
@@ -0,0 +1,37 @@
+    const browserData = [
+      { 
+        browser: 'Chrome', 
+        color: 'rgba(75, 192, 192, 1)',
+        users: 150,
+        marketshare: 70,
+        versionData: [
+          {version: 'v5', users: 10},
+          {version: 'v6', users: 20},
+          {version: 'v7', users: 30},
+          {version: 'v8', users: 60},
+          {version: 'v9', users: 20},
+        ],
+      },
+      { 
+        browser: 'FireFox',
+        color: 'rgba(255, 26, 104, 1)',
+        users: 25,
+        marketshare: 24,
+        versionData: [
+          {version: 'V3.1', users: 10},
+          {version: 'v3.2', users: 10},
+          {version: 'v3.3', users: 5},
+        ],
+      },    
+      { 
+        browser: 'Safari', 
+        color: 'rgba(54, 162, 235, 1)', 
+        users: 30,
+        marketshare: 26,
+        versionData: [
+          {version: 'Web 9', users: 10},
+          {version: 'Web 10', users: 10},
+          {version: 'Web 11', users: 10},
+        ],
+      }   
+    ];
